d8f1281c3aa9a9698b429ea958e0f516a8953876
Derek Troy-West <derek@troywest.com>
2019-05-11 17:43:32 +1200

    update

--- src/test/java/numbers/TopologyTest.java
+++ src/test/java/numbers/TopologyTest.java
@@ -31,14 +31,14 @@ public class TopologyTest extends TestCase {
     }
 
     Message[] testMessages = new Message[]{
-            new Message(1557125670789L, "GER", "85", -92, -30, new String[]{"eins", "null", "sechs"}),
+            new Message(1557125670789L, "GER", "085", -92, -30, new String[]{"eins", "null", "sechs"}),
             new Message(1557125670790L, "UXX", "XRAY"),
             new Message(1557125670794L, "MOR", "425", 77, 25, new String[]{".....", "----."}),
             new Message(1557125670795L, "UXX", "XRAY"),
             new Message(1557125670799L, "ENG", "NZ1", 166, -78, new String[]{"two"}),
             new Message(1557125670807L, "ENG", "159", -55, -18, new String[]{"three", "five"}),
             new Message(1557125670812L, "ENG", "426", 78, 26, new String[]{"six", "three"}),
-            new Message(1557125670814L, "GER", "85", -92, -30, new String[]{"drei", "neun"}),
+            new Message(1557125670814L, "GER", "085", -92, -30, new String[]{"drei", "neun"}),
             new Message(1557125670819L, "MOR", "425", 77, 25, new String[]{".----"}),
             new Message(1557125670824L, "ENG", "NZ1", 166, -78, new String[]{"two"}),
             new Message(1557125670827L, "ENG", "324", 27, 9, new String[]{"two", "nine"}),
@@ -46,10 +46,10 @@ public class TopologyTest extends TestCase {
             new Message(1557125670831L, "GER", "355", 42, 14, new String[]{"sieben"}),
             new Message(1557125670832L, "ENG", "159", -55, -18, new String[]{"three", "five"}),
             new Message(1557125670837L, "ENG", "426", 78, 26, new String[]{"one"}),
-            new Message(1557125670839L, "GER", "85", -92, -30, new String[]{"fünf", "fünf"}),
+            new Message(1557125670839L, "GER", "085", -92, -30, new String[]{"fünf", "fünf"}),
             new Message(1557125670840L, "GER", "505", 117, 39, new String[]{"eins", "null", "vier"}),
             new Message(1557125670841L, "GER", "487", 108, 36, new String[]{"eins", "null", "neun"}),
-            new Message(1557125670842L, "MOR", "20", -125, -41, new String[]{"...--"}),
+            new Message(1557125670842L, "MOR", "020", -125, -41, new String[]{"...--"}),
             new Message(1557125670843L, "GER", "199", -35, -11, new String[]{"eins", "vier"})};
 
     public void sendMessage(TopologyTestDriver driver, Message message) {
@@ -73,7 +73,7 @@ public class TopologyTest extends TestCase {
             sendMessages(driver, testMessages);
 
             Message[] expectedMessages = new Message[]{
-                    new Message(1557125670789L, "GER", "85", -92, -30, new String[]{"eins", "null", "sechs"}),
+                    new Message(1557125670789L, "GER", "085", -92, -30, new String[]{"eins", "null", "sechs"}),
                     new Message(1557125670794L, "MOR", "425", 77, 25, new String[]{".....", "----."}),
                     new Message(1557125670799L, "ENG", "NZ1", 166, -78, new String[]{"two"}),
                     new Message(1557125670807L, "ENG", "159", -55, -18, new String[]{"three", "five"}),
@@ -96,7 +96,7 @@ public class TopologyTest extends TestCase {
             sendMessages(driver, testMessages);
 
             Message[] expectedMessages = new Message[]{
-                    new Message(1557125670789L, "GER", "85", -92, -30, new String[]{"106"}),
+                    new Message(1557125670789L, "GER", "085", -92, -30, new String[]{"106"}),
                     new Message(1557125670794L, "MOR", "425", 77, 25, new String[]{"59"}),
                     new Message(1557125670799L, "ENG", "NZ1", 166, -78, new String[]{"2"}),
                     new Message(1557125670807L, "ENG", "159", -55, -18, new String[]{"35"}),
@@ -118,12 +118,12 @@ public class TopologyTest extends TestCase {
         try (TopologyTestDriver driver = new TopologyTestDriver(builder.build(), Topology.config)) {
             sendMessages(driver, testMessages);
 
-            try (KeyValueIterator iterator = driver.getWindowStore("PT10S-Store").fetch("85", (1557125670789L - 25000L), (1557125670789L + 100000L))) {
+            try (KeyValueIterator iterator = driver.getWindowStore("PT10S-Store").fetch("085", (1557125670789L - 25000L), (1557125670789L + 100000L))) {
                 Message expected = new Message() {
                     {
                         setTime(1557125670789L);
                         setType("GER");
-                        setName("85");
+                        setName("085");
                         setLongitude(-92);
                         setLatitude(-30);
                         setContent(new String[]{"106", "39", "55"});
